fca0a18eb9853b2a1370262d9ef930c90947f7f4
Jasmine <jazniec17@live.com>
2025-02-09 21:04:21 -0600

    feat: finish submission

--- scripts/index.js
+++ scripts/index.js
@@ -54,7 +54,6 @@ const addPostCaptionInput = addPostModal.querySelector(
 const addPostForm = addPostModal.querySelector(".modal__form");
 
 function getCardElement(data) {
-  console.log(data);
   const cardElement = cardTemplate.content
     .querySelector(".card")
     .cloneNode(true);
@@ -86,7 +85,10 @@ function handleEditFormSubmit(evt) {
 
 function handleAddPostSubmit(evt) {
   evt.preventDefault();
-  const inputValues = { name: "", link: "" };
+  const inputValues = {
+    name: addPostCaptionInput.value,
+    link: addPostLinkInput.value,
+  };
   const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
   closeModal(addPostModal);
